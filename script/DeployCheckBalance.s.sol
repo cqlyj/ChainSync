f2e3a41f017ed89e1610f5fbd71f0045b1b5b878
cqlyj <luoyingjie0721@gmail.com>
2024-12-08 20:24:11 +0800

    add DeployCheckBalance script for deploying CheckBalance contract

---
+++ script/DeployCheckBalance.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {CheckBalance} from "../src/CheckBalance.sol";
+
+contract DeployCheckBalance is Script {
+    HelperConfig public helperConfig;
+    CheckBalance public checkBalance;
+
+    function run() public {
+        (
+            string memory chainBaseUrl,
+            address tokenAddress,
+            address subscriber,
+            address router,
+            bytes32 donID
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        checkBalance = new CheckBalance(
+            chainBaseUrl,
+            tokenAddress,
+            subscriber,
+            router,
+            donID
+        );
+        vm.stopBroadcast();
+    }
+}
