747a6cb596c8a7cf7b062a45b25193e01795114d
cqlyj <luoyingjie0721@gmail.com>
2024-12-12 21:37:36 +0800

    add GetSignedMessage script for retrieving and logging signed messages from AmoyReceiver contract

---
+++ script/GetSignedMessage.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {AmoyReceiver} from "../src/AmoyReceiver.sol";
+
+contract GetSignedMessage is Script {
+    AmoyReceiver amoyReceiver;
+
+    function getSignedMessage(address mostRecentlyDeployed) public {
+        amoyReceiver = AmoyReceiver(mostRecentlyDeployed);
+        bytes memory signedMessage = amoyReceiver.getSignedMessage();
+        console.log("Signed message: ");
+        console.logBytes(signedMessage);
+    }
+
+    function run() public {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "AmoyReceiver",
+            block.chainid
+        );
+        console.log("Most recently deployed address: ", mostRecentlyDeployed);
+
+        getSignedMessage(mostRecentlyDeployed);
+    }
+}
