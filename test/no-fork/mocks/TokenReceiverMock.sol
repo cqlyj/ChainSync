a4df58f635cd2fcabb70e0946c6b2c5700dd6c97
cqlyj <luoyingjie0721@gmail.com>
2024-12-19 17:58:52 +0800

    add TokenReceiverMock contract for testing token reception

---
+++ test/no-fork/mocks/TokenReceiverMock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+/// @title TokenReceiverMock
+/// @author Luo Yingjie
+/// @notice This is a mock contract for receiving tokens.
+contract TokenReceiverMock {
+    receive() external payable {}
+}
