49c25c46929ce114be9cdba6a3e693f514cf863d
cqlyj <luoyingjie0721@gmail.com>
2024-12-20 18:52:44 +0800

    add AmoyReceiverSignedMessage library for signed message handling

---
+++ src/library/AmoyReceiverSignedMessage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+library AmoyReceiverSignedMessage {
+    struct SignedMessage {
+        uint64 chainSelector; // Include the target chain selector
+        address user; // Signer's address
+        address token; // Token to transfer
+        uint256 amount; // Amount to transfer
+        address transferContract; // Contract address for transfer, first transfer to this contract, then router will transfer to the target chain
+        address router; // Router address for approval
+        uint256 nonce; // Nonce for replay protection
+        uint256 expiry; // Expiry timestamp
+    }
+}
