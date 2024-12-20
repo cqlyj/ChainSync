a53bf86ea4805679b8188cb5d3df883128cda9cf
cqlyj <luoyingjie0721@gmail.com>
2024-12-20 18:53:12 +0800

    refactor MessageTransferTest to use AmoyReceiverSignedMessage for message signing and validation

--- test/no-fork/MessageTransferTest.t.sol
+++ test/no-fork/MessageTransferTest.t.sol
@@ -2,18 +2,24 @@
 pragma solidity 0.8.26;
 
 import {Test} from "forge-std/Test.sol";
-import {CCIPLocalSimulator, IRouterClient, LinkToken} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
+import {CCIPLocalSimulator, IRouterClient, LinkToken, BurnMintERC677Helper} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
 import {SepoliaSender} from "src/SepoliaSender.sol";
 import {AmoyReceiver} from "src/AmoyReceiver.sol";
+import {AmoyReceiverSignedMessage} from "src/library/AmoyReceiverSignedMessage.sol";
 
 contract MessageTransferTest is Test {
     CCIPLocalSimulator public ccipLocalSimulator;
     uint64 public destinationChainSelector;
+    BurnMintERC677Helper public CCIPBnM;
 
     SepoliaSender public sepoliaSender;
     AmoyReceiver public amoyReceiver;
 
-    address public USER = makeAddr("USER");
+    address user;
+    uint256 userPrivateKey;
+
+    uint256 constant AMOUNT_CCIPBNM = 1e18;
+    IRouterClient destinationRouter;
 
     function setUp() public {
         ccipLocalSimulator = new CCIPLocalSimulator();
@@ -21,40 +27,63 @@ contract MessageTransferTest is Test {
         (
             uint64 chainSelector,
             IRouterClient sourceRouter,
-            IRouterClient destinationRouter,
+            IRouterClient _destinationRouter,
             ,
             LinkToken link,
-            ,
+            BurnMintERC677Helper ccipBnM,
 
         ) = ccipLocalSimulator.configuration();
 
         destinationChainSelector = chainSelector;
+        CCIPBnM = ccipBnM;
+        destinationRouter = _destinationRouter;
 
         sepoliaSender = new SepoliaSender(address(sourceRouter), address(link));
         amoyReceiver = new AmoyReceiver(address(destinationRouter));
+
+        (user, userPrivateKey) = makeAddrAndKey("user");
     }
 
-    function testMessageTransferWithLinkTokenPaid() public {
+    function testMessageTransferPassTheValidation() public {
         ccipLocalSimulator.requestLinkFromFaucet(
             address(sepoliaSender),
             5 ether
         );
 
-        bytes memory signedMessage = abi.encodePacked("Hello, Amoy!");
+        AmoyReceiverSignedMessage.SignedMessage
+            memory signedMessage = AmoyReceiverSignedMessage.SignedMessage({
+                chainSelector: destinationChainSelector,
+                user: user,
+                token: address(CCIPBnM),
+                amount: AMOUNT_CCIPBNM,
+                transferContract: address(amoyReceiver),
+                router: address(destinationRouter),
+                nonce: 0,
+                expiry: block.timestamp + 1 days
+            });
 
+        bytes32 digest = amoyReceiver.getMessageHash(signedMessage);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        bytes memory encodedSignedMessage = abi.encode(
+            user,
+            signedMessage,
+            v,
+            r,
+            s
+        );
+
+        vm.pauseGasMetering();
         bytes32 messageId = sepoliaSender.sendMessage(
             destinationChainSelector,
             address(amoyReceiver),
-            signedMessage
+            encodedSignedMessage
         );
 
-        bytes memory receivedMessage = amoyReceiver.getSignedMessage();
         bytes32 lastMessageId = amoyReceiver.getMessageId();
-
-        string memory expectedMessage = "Hello, Amoy!";
-        string memory actualMessage = abi.decode(receivedMessage, (string));
+        bytes memory receivedMessage = amoyReceiver.getSignedMessage();
 
         assertEq(messageId, lastMessageId);
-        assertEq(expectedMessage, actualMessage);
+        assertEq(encodedSignedMessage, receivedMessage);
     }
 }
