0be6c81c175179f3f171cc72882aae6f7986c04a
cqlyj <luoyingjie0721@gmail.com>
2024-12-22 22:34:00 +0800

    update DeployAmoyReceiver script

--- script/DeployAmoyReceiver.s.sol
+++ script/DeployAmoyReceiver.s.sol
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {AmoyReceiver} from "../src/AmoyReceiver.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract DeployAmoyReceiver is Script {
     address constant AMOY_ROUTER = 0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2;
 
     function run() public {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "AmoyTokenTransfer",
+            block.chainid
+        );
+        console.log("Most recently deployed address: ", mostRecentlyDeployed);
         AmoyReceiver amoyReceiver;
 
         vm.startBroadcast();
-        amoyReceiver = new AmoyReceiver(AMOY_ROUTER);
+        amoyReceiver = new AmoyReceiver(AMOY_ROUTER, mostRecentlyDeployed);
         vm.stopBroadcast();
     }
 }
