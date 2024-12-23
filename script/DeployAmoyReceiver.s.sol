085a14470add674b98675e0e4b0f67337af4e19e
cqlyj <luoyingjie0721@gmail.com>
2024-12-23 08:45:58 +0800

    update DeployAmoyReceiver to use AMOY_LINK instead of most recently deployed address

--- script/DeployAmoyReceiver.s.sol
+++ script/DeployAmoyReceiver.s.sol
@@ -7,17 +7,13 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract DeployAmoyReceiver is Script {
     address constant AMOY_ROUTER = 0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2;
+    address constant AMOY_LINK = 0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904;
 
     function run() public {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "AmoyTokenTransfer",
-            block.chainid
-        );
-        console.log("Most recently deployed address: ", mostRecentlyDeployed);
         AmoyReceiver amoyReceiver;
 
         vm.startBroadcast();
-        amoyReceiver = new AmoyReceiver(AMOY_ROUTER, mostRecentlyDeployed);
+        amoyReceiver = new AmoyReceiver(AMOY_ROUTER, AMOY_LINK);
         vm.stopBroadcast();
     }
 }
