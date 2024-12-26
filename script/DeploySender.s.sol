ac456ecab5676e2db6c63c764261ddbd839fcb4f
cqlyj <luoyingjie0721@gmail.com>
2024-12-26 13:03:58 +0800

    rename DeploySepoliaSender script to DeploySender and update LINK address reference

--- script/DeploySender.s.sol
+++ script/DeploySender.s.sol
@@ -3,20 +3,20 @@ pragma solidity 0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {SepoliaSender} from "../src/SepoliaSender.sol";
+import {Sender} from "../src/Sender.sol";
 
-contract DeploySepoliaSender is Script {
-    address constant SEPOLIA_LINK_ADDRESS =
-        0x779877A7B0D9E8603169DdbD7836e478b4624789;
+contract DeploySender is Script {
+    address constant AMOY_LINK_ADDRESS =
+        0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904;
 
     function run() public {
         HelperConfig helperConfig = new HelperConfig();
-        SepoliaSender sepoliaSender;
+        Sender sender;
         (, , , , , address ccipRouter) = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast();
 
-        sepoliaSender = new SepoliaSender(ccipRouter, SEPOLIA_LINK_ADDRESS);
+        sender = new Sender(ccipRouter, AMOY_LINK_ADDRESS);
 
         vm.stopBroadcast();
     }
