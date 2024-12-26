082ed3afab13fe6bfe3ba817d44fb1f08a244f2c
cqlyj <luoyingjie0721@gmail.com>
2024-12-26 12:59:02 +0800

    refactor GetSignedMessage script to use Receiver contract instead of AmoyReceiver

--- script/GetSignedMessage.s.sol
+++ script/GetSignedMessage.s.sol
@@ -3,21 +3,21 @@ pragma solidity 0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
-import {AmoyReceiver} from "../src/AmoyReceiver.sol";
+import {Receiver} from "../src/Receiver.sol";
 
 contract GetSignedMessage is Script {
-    AmoyReceiver amoyReceiver;
+    Receiver receiver;
 
     function getSignedMessage(address mostRecentlyDeployed) public {
-        amoyReceiver = AmoyReceiver(payable(mostRecentlyDeployed));
-        bytes memory signedMessage = amoyReceiver.getSignedMessage();
+        receiver = Receiver(payable(mostRecentlyDeployed));
+        bytes memory signedMessage = receiver.getSignedMessage();
         console.log("Signed message: ");
         console.logBytes(signedMessage);
     }
 
     function run() public {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "AmoyReceiver",
+            "Receiver",
             block.chainid
         );
         console.log("Most recently deployed address: ", mostRecentlyDeployed);
