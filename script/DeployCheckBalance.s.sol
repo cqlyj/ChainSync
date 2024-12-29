357bf77cc4ed26e8f0ce84ddce207ce49f97b86d
cqlyj <luoyingjie0721@gmail.com>
2024-12-29 21:22:39 +0800

    feat: update DeployCheckBalance script to improve variable naming and add deployment logs

--- script/DeployCheckBalance.s.sol
+++ script/DeployCheckBalance.s.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {CheckBalance} from "../src/CheckBalance.sol";
 
 contract DeployCheckBalance is Script {
     function run() public {
-        HelperConfig helperConfig = new HelperConfig();
         CheckBalance checkBalance;
-        (, , , address router, bytes32 donID, ) = helperConfig
+        HelperConfig helperConfig = new HelperConfig();
+        (, address functionRouter, bytes32 donID, , , ) = helperConfig
             .activeNetworkConfig();
 
         vm.startBroadcast();
-        checkBalance = new CheckBalance(router, donID);
+        checkBalance = new CheckBalance(functionRouter, donID);
         vm.stopBroadcast();
+
+        console.log("CheckBalance deployed at chain ID: ", block.chainid);
+        console.log(
+            "CheckBalance deployed at address: ",
+            address(checkBalance)
+        );
     }
 }
