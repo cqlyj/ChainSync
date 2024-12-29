500e163afa777b14c3a92089f62cc2e693bda4bb
cqlyj <luoyingjie0721@gmail.com>
2024-12-29 21:25:56 +0800

    feat: update DeploySender script to use dynamic link address and improve logging

--- script/DeploySender.s.sol
+++ script/DeploySender.s.sol
@@ -1,23 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {Sender} from "../src/Sender.sol";
 
 contract DeploySender is Script {
-    address constant AMOY_LINK_ADDRESS =
-        0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904;
-
     function run() public {
         HelperConfig helperConfig = new HelperConfig();
         Sender sender;
-        (, , , , , address ccipRouter) = helperConfig.activeNetworkConfig();
+        (, , , address router, , , address link) = helperConfig
+            .activeNetworkConfig();
 
         vm.startBroadcast();
-
-        sender = new Sender(ccipRouter, AMOY_LINK_ADDRESS);
-
+        sender = new Sender(router, link);
         vm.stopBroadcast();
+
+        console.log("Sender deployed at chainid: ", block.chainid);
+        console.log("Sender address: ", address(sender));
     }
 }
