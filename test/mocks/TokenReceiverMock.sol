c6ab4782e73f29f7b29219b8e62c29aa6d5da9c0
cqlyj <luoyingjie0721@gmail.com>
2024-12-31 00:00:32 +0800

    feat: remove obsolete fork test contracts and mock for cleaner codebase

--- test/mocks/TokenReceiverMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
-
-/// @title TokenReceiverMock
-/// @author Luo Yingjie
-/// @notice This is a mock contract for receiving tokens.
-contract TokenReceiverMock {
-    receive() external payable {}
-}
