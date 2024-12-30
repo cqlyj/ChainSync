f2a7c86ff4882a44a032750755c6b47668c12991
cqlyj <luoyingjie0721@gmail.com>
2024-12-30 22:50:44 +0800

    feat: enhance ReceiverSignedMessage library with detailed documentation for SignedMessage structure

--- src/library/ReceiverSignedMessage.sol
+++ src/library/ReceiverSignedMessage.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+/// @title ReceiverSignedMessage
+/// @author Luo Yingjie
+/// @notice This library defines the signed message structure for the receiver
+
 library ReceiverSignedMessage {
+    /// @notice The signed message structure for the receiver
+    /// @param chainSelector The target chain selector
+    /// @param user The signer's address
+    /// @param token The token on the destination chain to transfer
+    /// @param amount The amount to transfer
+    /// @param transferContract The contract address for transfer, first transfer to this contract, then router will transfer to the target chain => Receiver
+    /// @param router The router address for approval => destination chain's router
+    /// @param nonce The nonce for replay protection
+    /// @param expiry The expiry timestamp
+
     struct SignedMessage {
         uint64 chainSelector; // Include the target chain selector
         address user; // Signer's address
